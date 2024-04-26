af1fe90dbc01d00b9dba6e2dbaa2fd9b02d13323
Reisa Prasaptaraya <r.prasaptaraya@gmail.com>
2024-04-27 00:03:18 +0700

    [features] add global database mysql and redis setup

---
+++ app/database/db.go
@@ -0,0 +1,23 @@
+package database
+
+import (
+	"bwastartup/app/entity"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func Connect() {
+	var err error
+
+	DB, err = gorm.Open(mysql.Open("root:root@tcp(db:3306)/ambassador"), &gorm.Config{})
+
+	if err != nil {
+		panic("Could not connect with the database!")
+	}
+}
+
+func AutoMigrate() {
+	DB.AutoMigrate(entity.User{}, entity.Campaign{}, entity.CampaignImage{}, entity.Transactions_table{})
+}
--- go.mod
+++ go.mod
@@ -12,6 +12,9 @@ require (
 
 require (
 	filippo.io/edwards25519 v1.1.0 // indirect
+	github.com/cespare/xxhash/v2 v2.3.0 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/go-redis/redis/v8 v8.11.5 // indirect
 	github.com/go-sql-driver/mysql v1.8.1 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
