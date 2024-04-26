b30c2a5b5187da5a6ec625c9c0afa343745d5e3b
Reisa Prasaptaraya <r.prasaptaraya@gmail.com>
2024-04-26 11:26:26 +0700

    [features] generate database with gorm gen

--- Makefile
+++ Makefile
@@ -6,5 +6,6 @@ migrateup:
 	migrate -path sql/migration -database "mysql://root:secret@tcp(localhost:3306)/bwa?x-tls-insecure-skip-verify=false" -verbose up
 migratedown:
 	migrate -path sql/migration -database "mysql://root:secret@tcp(localhost:3306)/bwa?x-tls-insecure-skip-verify=false" -verbose down
-
-.PHONY: app stopapp migrateup migratedown
+test :
+	go test -v -cover ./...
+.PHONY: app stopapp migrateup migratedown test
---
+++ generatedb.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"gorm.io/driver/mysql"
+	"gorm.io/gen"
+	"gorm.io/gorm"
+	"log"
+)
+
+func main() {
+	g := gen.NewGenerator(gen.Config{
+		OutPath:        "./query",
+		WithUnitTest:   true,
+		FieldNullable:  true,
+		FieldCoverable: true,
+		Mode:           gen.WithoutContext | gen.WithDefaultQuery | gen.WithQueryInterface, // generate mode
+	})
+
+	dsn := "root:secret@tcp(127.0.0.1:3306)/bwa?charset=utf8mb4&parseTime=True&loc=Local"
+	gormdb, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	fmt.Println("connection database success !!")
+	g.UseDB(gormdb) // reuse your gorm db
+	g.ApplyBasic(
+		g.GenerateModel("users"),
+		g.GenerateModel("transactions"),
+		g.GenerateModel("campaigns"),
+		g.GenerateModel("campaign_images"),
+	)
+	g.Execute()
+}
