1e816f2d537137dd9a1eaa6ce3861ead760fbe7a
Reisa Prasaptaraya <r.prasaptaraya@gmail.com>
2024-04-28 19:00:42 +0700

    [etc] change name handler with controller

--- routes/v1/user.routes.go
+++ routes/v1/user.routes.go
@@ -2,7 +2,7 @@ package v1
 
 import (
 	"bwastartup/src/auth"
-	"bwastartup/src/handler"
+	"bwastartup/src/controller"
 	"bwastartup/src/helper/mysql"
 	"bwastartup/src/user"
 	"github.com/gin-gonic/gin"
@@ -12,7 +12,7 @@ func UserRoutes(api *gin.RouterGroup) {
 	userRepository := user.NewRepository(mysql.DB)
 	userService := user.NewService(userRepository)
 	authService := auth.NewService()
-	userHandler := handler.NewUserHandler(userService, authService)
+	userHandler := controller.NewUserHandler(userService, authService)
 
 	// Per route middleware, you can add as many as you desire.
 	//api.GET("/benchmark", MyBenchLogger(), benchEndpoint)
